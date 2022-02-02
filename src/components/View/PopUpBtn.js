74b1f277ab06ba1320f921db2bd520f3aaf27198
Nikul <=>
2022-02-02 17:04:07 +0530

    css changes

--- src/components/View/PopUpBtn.js
+++ src/components/View/PopUpBtn.js
@@ -46,7 +46,7 @@ const pop = () => {
 const PopUpBtn = () => {
     return (
         <>
-            <div class="p-fixed z-5 transition-all" style={{ bottom: "15px", left: "15px", position: "fixed", background: "#3eb49c", cursor: "pointer" }} onClick={()=>pop()}>
+            <div class="p-fixed z-5 transition-all" style={{ bottom:/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone/i.test(navigator.userAgent) ? "55px":"15px", left: "15px", position: "fixed", background: "#3eb49c", cursor: "pointer" }} onClick={()=>pop()}>
                 <div class="transition-all d-inline-block rounded sectionBg fwb text-mint f-20 shadow p-3 line-height-smaller p-relative animated-pastel-gradient mail-button" style={{ border: "3px solid white", borderRadius: "100px !important" }}>
                     <div class="animated tada d-inline-block infinite " style={{ animationDuration: "2s" }}>👋</div>
                 </div>
