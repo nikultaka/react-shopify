95ffe4e37de32752e925ce06f362ce3c246f63a6
Nikul <nikultaka@gmail.com>
2022-02-09 19:16:47 +0530

    new changes added

--- src/components/View/layout/Slider.js
+++ src/components/View/layout/Slider.js
@@ -3,27 +3,51 @@ const Slider = () =>{
     return(
         <section className="slider_section">
         <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-lg-10">   
-              <a href="#" className="slidPrv_1">
-                <img src={helper.ImagePath+"arrw_lft.svg"} alt="a" className="img-fluid"/>
-              </a>
-              <a href="#" className="slidNext_1">
-                <img src={helper.ImagePath+"arw_rgt.svg"} alt="a" className="img-fluid"/>
-              </a>
-              <div className="vdo_sliders">
-                <a href="#">
-                  <img src={helper.ImagePath+"slider_img.svg"} alt="a" className="img-fluid"/>
-                </a>
-                <a href="#">
-                  <img src={helper.ImagePath+"slider_img.svg"} alt="a" className="img-fluid"/>
-                </a>
-                <a href="#">
-                  <img src={helper.ImagePath+"slider_img.svg"} alt="a" className="img-fluid"/>
-                </a>
+          
+             
+            {
+              (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Windows Phone/i.test(navigator.userAgent)) ?
+              <div className="row justify-content-center">
+                <div className="col-lg-10">
+                  <div className="vdo_sliders">
+                    <a href="#">
+                      <img src={helper.ImagePath+"slider_img.svg"} alt="a" className="img-fluid"/>
+                    </a>
+                    <a href="#">
+                      <img src={helper.ImagePath+"slider_img.svg"} alt="a" className="img-fluid"/>
+                    </a>
+                    <a href="#">
+                      <img src={helper.ImagePath+"slider_img.svg"} alt="a" className="img-fluid"/>
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+              :
+              <div className="row justify-content-center">
+                <div className="col-lg-10">
+                  <a href="#" className="slidPrv_1">
+                    <img src={helper.ImagePath+"arrw_lft.svg"} alt="a" className="img-fluid"/>
+                  </a>
+                  <a href="#" className="slidNext_1">
+                    <img src={helper.ImagePath+"arw_rgt.svg"} alt="a" className="img-fluid"/>
+                  </a>
+                  <div className="vdo_sliders">
+                    <a href="#">
+                      <img src={helper.ImagePath+"slider_img.svg"} alt="a" className="img-fluid"/>
+                    </a>
+                    <a href="#">
+                      <img src={helper.ImagePath+"slider_img.svg"} alt="a" className="img-fluid"/>
+                    </a>
+                    <a href="#">
+                      <img src={helper.ImagePath+"slider_img.svg"} alt="a" className="img-fluid"/>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            }   
+             
+              
+          
         </div>
       </section>
     )
